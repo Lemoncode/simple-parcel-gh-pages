1dee3c85b60152a90291b03d623fd4ae4cd2da5d
Braulio Diez Botella <braulio.diez@gmail.com>
2020-06-13 20:41:18 +0200

    new site

--- README.md
+++
@@ -1,1 +0,0 @@
-# simple-parcel-gh-pages
--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { SumComponent } from "./sum";
-
-ReactDOM.render(<SumComponent />, document.getElementById("root"));
